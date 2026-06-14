3291a2ed4386984fca9aa624dc271563a98a921e
Masaki Ohara <masakingu921@gmail.com>
2026-06-14 21:54:51 +0900

    プロフィール画像を反映

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -9,27 +9,36 @@ module Api
     module Auth
       class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
-  def create
-    super do |resource|
-      if resource.persisted?
-        render json: {
-          status: "success",
-          message: "ユーザー登録が完了しました"
-        }, status: :created and return
+        def create
+          super do |resource|
+            if resource.persisted?
+              image_url = resource.image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(resource.image, only_path: true) : nil
+              render json: {
+                status: "success",
+                message: "ユーザー登録が完了しました",
+                # 👇 render json: { } の中に data を入れます
+                data: {
+                  id: resource.id,
+                  name: resource.name,
+                  email: resource.email,
+                  image: image_url
+                }
+              }, status: :created and return
+            end
+          end
+        end
+
+        private
+
+        def sign_up_params
+          params.permit(:name, :email, :password, :password_confirmation, :image)
+        end
+
+        def account_update_params
+          params.permit(:name, :email, :password, :password_confirmation, :image)
+        end
+
       end
     end
   end
-
-  private
-
-  def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation, :image)
-  end
-
-  def account_update_params
-    params.permit(:name, :email, :password, :password_confirmation, :image)
-  end
-end
-end
-end
 end
